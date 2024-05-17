5385850ba14666876e60ff4e3d2202d2a86a94a8
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-05-17 18:04:48 +0530

    ledger enteries

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
         <link href="{{asset('/assets/css/styles.css')}}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-default" style="background-color:#e2d1e5;">
+    <body class="bg-dark" style="background-color:#e2d1e5;">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
