6a51bcd786d0438a02796ccbd0ae1bbaa080c3c2
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-03 15:20:24 +0530

    beep disabled

--- database/migrations/2024_07_02_184512_create_base_currencies_table.php
+++ database/migrations/2024_07_02_184512_create_base_currencies_table.php
@@ -15,7 +15,7 @@ class CreateBaseCurrenciesTable extends Migration
     {
         Schema::create('base_currencies', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('base_id');
+            $table->unsignedBigInteger('base_id')->index(); 
             $table->string('name');
             $table->boolean('used')->default(false);
             $table->integer('open_day');
