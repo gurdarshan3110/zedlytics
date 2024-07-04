10710215ae059d836fe7c7282bd28d86a9a39450
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-05 02:19:23 +0530

    Positions Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -114,7 +114,7 @@ class DashboardController extends Controller
                             ->sum('closeAmount');
                         $changeQtyShort = $changeQtyShort+$changeQtyShort1;
 
-                        $changeQty = $changeQtyLong + $changeQtyShort;
+                        $changeQty = $changeQtyLong. '+'. $changeQtyShort;
                     }
 
                     $firstPosition = $group->first();
