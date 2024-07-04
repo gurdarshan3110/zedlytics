27ffe77209d216638c51f73b9c07a88aadc81cca
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-05 01:56:09 +0530

    Positions Chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -81,7 +81,12 @@ class DashboardController extends Controller
 
                     // Calculate metrics for all but the last entry
                     $longQty = $allButLast->where('posType', 1)->sum('openAmount');
+                    $longQty1 = $allButLast->where('posType', 1)->sum('closeAmount');
+                    $longQty = $longQty - $longQty1;
+
                     $shortQty = $allButLast->where('posType', 2)->sum('openAmount');
+                    $shortQty1 = $allButLast->where('posType', 2)->sum('closeAmount');
+                    $shortQty = $shortQty - $shortQty1;
                     $longDeals = $allButLast->where('posType', 1)->count();
                     $shortDeals = $allButLast->where('posType', 2)->count();
                     $netQty = round($longQty, 2) + round($shortQty, 2);
