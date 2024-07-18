0f26500c3ae7e8535b244334a63f3c233130ae04
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-18 15:54:28 +0400

    transaction log

--- routes/console.php
+++ routes/console.php
@@ -13,6 +13,6 @@ use App\Jobs\CreateNewClientsJob;
 
 Schedule::job(new FetchOpenPositionsJob, 'default')->everyFourMinutes();
 Schedule::job(new CreateClientAndAccountJob, 'default')->everyFiveMinutes();
-Schedule::job(new TransactionLogJob, 'default')->everyTenMinutes();
-Schedule::job(new CreateNewClientsJob, 'default')->everyFifteenMinutes();
+Schedule::job(new TransactionLogJob, 'default')->everyTwoMinutes();
+Schedule::job(new CreateNewClientsJob, 'default')->everyTwoMinutes();
 //Schedule::job(new FetchOpenPositionsJob, 'openpositions', 'sqs')->everyFiveMinutes();
