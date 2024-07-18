a9e19fb7a5e302d2c52e352acc131ad98f5f389c
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-18 14:18:47 +0400

    transaction log

--- app/Jobs/CreateNewClientsJob.php
+++ app/Jobs/CreateNewClientsJob.php
@@ -8,12 +8,12 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
-use App\Models\TxnLog;
+use App\Models\Client;
 use App\Models\CronJob;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 
-class TransactionLogJob implements ShouldQueue
+class CreateNewClientsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -37,22 +37,23 @@ class TransactionLogJob implements ShouldQueue
             // Login to the API once
             CronJob::create(['cron_job_name' => 'Update Transaction Log']);
 
-            $currentDate = Carbon::now()->endOfDay()->toDateTimeString();
-            $response = Http::withToken($this->token)->get("https://bestbullapi.arktrader.io/api/apigateway/admin/public/api/v1/user/20597/transactionLogs?fromDate=2020-01-01 00:00:00&toDate=".$currentDate."&ticketOrderId=&trxLogActionTypeId=&trxLogTransTypeId=&trxSubTypeId=&ipAddress=&createdById=");
+            $startDate = Carbon::now()->subMinutes(15);
+            $endDate = Carbon::now()->endOfDay()->toDateTimeString();
+            $response = Http::withToken($this->token)->get("https://bestbullapi.arktrader.io/api/apigateway/admin/public/api/v1/report/users/details/".$this->clientTreeUserIdNode."?fromDate=".$startDate."&toDate=".$endDate);
 
             // Handle the response as needed
             if ($response->successful()) {
 
                 $clientData = $response->json()['data'];
-                $trxLog = TrxLog::updateOrCreate(
-                    ['id' => $clientData['id']],
+                $trxLog = Client::updateOrCreate(
+                    ['user_id' => $clientData['userID']],
                     $clientData
                 );
 
             } else {
                 // Handle API call failure
                 // Log the error or take appropriate actions
-                \Log::error("Failed to update transaction log: " . $response->body());
+                \Log::error("Failed to update client " . $response->body());
             }
             
         } catch (\Exception $e) {
