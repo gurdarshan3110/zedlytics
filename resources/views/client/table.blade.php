53c60a9a59b984a9c529b6537d86f2791ae7e734
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-27 15:34:32 +0400

    API issues

--- resources/views/client/table.blade.php
+++ resources/views/client/table.blade.php
@@ -25,71 +25,72 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
     <script type="text/javascript">
-    var table = $('#record-table').DataTable({
-        "columnDefs": [
-            { "width": "7%", "targets": 0 },
-            { "width": "10%", "targets": 1 },
-            { "width": "7%", "targets": 2 },
-            { "width": "20%", "targets": 3 },
-            { "width": "20%", "targets": 4 },
-            { "width": "7%", "targets": 5 },
-            { "width": "19%", "targets": 6 },
-            { "width": "10%", "targets": 7 },
-        ],
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: "/{{$url}}/list",
-            type: 'GET',
-            data: function (d) {
-                d.status = 0; // Add the status filter
+        var table = $('#record-table').DataTable({
+            "columnDefs": [
+                { "width": "7%", "targets": 0 },
+                { "width": "10%", "targets": 1 },
+                { "width": "7%", "targets": 2 },
+                { "width": "20%", "targets": 3 },
+                { "width": "20%", "targets": 4 },
+                { "width": "7%", "targets": 5 },
+                { "width": "19%", "targets": 6 },
+                { "width": "10%", "targets": 7 },
+            ],
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: "/{{$url}}/list",
+                type: 'GET',
+                data: function (d) {
+                    d.status = 0; // Add the status filter
+                }
+            },
+            columns: [
+                { data: 'brand', name: 'brand' },
+                { data: 'client_code', name: 'client_code' },
+                { data: 'username', name: 'username' },
+                { data: 'name', name: 'name' },
+                { data: 'email', name: 'email' },
+                { data: 'phone_no', name: 'phone_no' },
+                { data: 'rm', name: 'rm' },
+                { data: 'action', name: 'action', className: 'action', orderable: false, searchable: false },
+            ],
+            pageLength: 10,
+            dom: 'Blfrtip',
+            responsive: true,
+            scrollX: true,  // enables horizontal scrolling
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5',
+            ],
+            language: {
+                search: '',
+                searchPlaceholder: "Search {{$title}}",
+                paginate: {
+                    previous: '<i class="fa fa-angle-left"></i>',
+                    next: '<i class="fa fa-angle-right"></i>'
+                }
+            },
+            drawCallback: function () {
+                $('.dataTables_filter input').addClass('form-control form-control-solid w-250px');
+                $('.dt-buttons button').addClass('fs-7 active-menu-item text-light border-0');
+                $('.dt-buttons').addClass('mb-2');
+                $('.dt-button').addClass('btn-primary');
+                $('.paginate_button').addClass('fs-7');
+                $('.paginate_button.current').addClass('fs-7 active-menu-item');
+                $('.paginate_button.active-menu-item').removeClass('current');
             }
-        },
-        columns: [
-            { data: 'brand', name: 'brand' },
-            { data: 'client_code', name: 'client_code' },
-            { data: 'username', name: 'username' },
-            { data: 'name', name: 'name' },
-            { data: 'email', name: 'email' },
-            { data: 'phone_no', name: 'phone_no' },
-            { data: 'rm', name: 'rm' },
-            { data: 'action', name: 'action', className: 'action', orderable: false, searchable: false },
-        ],
-        pageLength: 10,
-        dom: 'Blfrtip',
-        responsive: true,
-        scrollX: true,  // enables horizontal scrolling
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5',
-        ],
-        language: {
-            search: '',
-            searchPlaceholder: "Search {{$title}}",
-            paginate: {
-                previous: '<i class="fa fa-angle-left"></i>',
-                next: '<i class="fa fa-angle-right"></i>'
-            }
-        },
-        drawCallback: function () {
-            $('.dataTables_filter input').addClass('form-control form-control-solid w-250px');
-            $('.dt-buttons button').addClass('fs-7 active-menu-item text-light border-0');
-            $('.dt-buttons').addClass('mb-2');
-            $('.dt-button').addClass('btn-primary');
-            $('.paginate_button').addClass('fs-7');
-            $('.paginate_button.current').addClass('fs-7 active-menu-item');
-            $('.paginate_button.active-menu-item').removeClass('current');
-        }
-    });
+        });
 
-    $('#filter').click(function(){
-        var min = $('#min').val();
-        var max = $('#max').val();
-        table.ajax.url('/agencies/list?min=' + min + '&max=' + max).load();
-    });
-</script>
+        $('#filter').click(function(){
+            var min = $('#min').val();
+            var max = $('#max').val();
+            table.ajax.url('/{{$url}}/list?min=' + min + '&max=' + max).load();
+        });
+    </script>
+
 
 
 @endpush
