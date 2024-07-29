f00f62ddd8364c90bf2bfa7af9518158ee0c48b5
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-29 16:51:40 +0400

    rms dashboard

--- app/Http/Controllers/RiskManagementController.php
+++ app/Http/Controllers/RiskManagementController.php
@@ -37,19 +37,19 @@ class RiskManagementController extends Controller
         $fname = self::FNAME;
         $date = Carbon::today()->toDateString();
         
-        $topTenWinners = TrxLog::select('userId','accountId')
+        $topTenWinners = TrxLog::select('userId','accountId','username')
             ->selectSub('SUM(closeProfit)', 'totalCloseProfit')
             ->whereDate('createdDate',$date)
             ->whereNotNull('closeProfit')
-            ->groupBy('userId','accountId')
+            ->groupBy('userId','accountId','username')
             ->orderBy('totalCloseProfit', 'desc')
             ->limit(10)
             ->get();
-        $topTenLossers = TrxLog::select('userId','accountId')
+        $topTenLossers = TrxLog::select('userId','accountId','username')
             ->selectSub('SUM(closeProfit)', 'totalCloseProfit')
             ->whereDate('createdDate',$date)
             ->whereNotNull('closeProfit')
-            ->groupBy('userId','accountId')
+            ->groupBy('userId','accountId','username')
             ->orderBy('totalCloseProfit', 'asc')
             ->limit(10)
             ->get();
@@ -64,19 +64,19 @@ class RiskManagementController extends Controller
     {
         $date = $request->input('date', Carbon::today()->toDateString());
 
-        $topTenWinners = TrxLog::select('userId','accountId')
+        $topTenWinners = TrxLog::select('userId','accountId','username')
             ->selectSub('SUM(closeProfit)', 'totalCloseProfit')
             ->whereDate('createdDate',$date)
             ->whereNotNull('closeProfit')
-            ->groupBy('userId','accountId')
+            ->groupBy('userId','accountId','username')
             ->orderBy('totalCloseProfit', 'desc')
             ->limit(10)
             ->get();
-        $topTenLosers = TrxLog::select('userId','accountId')
+        $topTenLosers = TrxLog::select('userId','accountId','username')
             ->selectSub('SUM(closeProfit)', 'totalCloseProfit')
             ->whereDate('createdDate',$date)
             ->whereNotNull('closeProfit')
-            ->groupBy('userId','accountId')
+            ->groupBy('userId','accountId','username')
             ->orderBy('totalCloseProfit', 'asc')
             ->limit(10)
             ->get();
