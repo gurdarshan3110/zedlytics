77a6271ce11a9106f88378c727f3a63a5557d26b
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-30 11:54:31 +0400

    transaction log changes and report by timezone

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -66,8 +66,8 @@
                             <td>{{ $position['longQty'] }}</td>
                             <td>{{ $position['shortDeals'] }}</td>
                             <td>{{ $position['shortQty'] }}</td>
-                            <td class="{{(($position['netQty']>0)?'bg-brand text-light':(($position['netQty']<0)?'bg-danger text-light':''))}}">{{ $position['netQty'] }}</td>
-                            <td class="{{ (($position['changeQty'] > 0) ? 'bg-brand text-light' : (($position['changeQty'] == 0) ? 'text-dark' : 'bg-danger text-light')) }}">
+                            <td class="{{(($position['netQty']>0)?'bg-success text-light':(($position['netQty']<0)?'bg-danger text-light':''))}}">{{ $position['netQty'] }}</td>
+                            <td class="{{ (($position['changeQty'] > 0) ? 'bg-success text-light' : (($position['changeQty'] == 0) ? 'text-dark' : 'bg-danger text-light')) }}">
                                 {{ $position['changeQty'] }}
                             </td>
                             <td>{{ $position['lastChange'] }}</td>
