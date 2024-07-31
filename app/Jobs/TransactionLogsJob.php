a34990f50f4c10f71983ff0e843e2094c41ea4df
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-07-31 09:28:19 +0400

    cron_job testing

--- app/Jobs/TransactionLogsJob.php
+++ app/Jobs/TransactionLogsJob.php
@@ -93,13 +93,15 @@ class TransactionLogsJob implements ShouldQueue
         }
 
         if ($start_time != null) {
-            $start_time = Carbon::now()->subMinutes(2)->format('Y-m-d H:i:s');
+            //$start_time = Carbon::now()->subMinutes(2)->format('Y-m-d H:i:s');
             //$start_time = '2024-07-31 09:00:00';
+            $start_time = Carbon::now('Asia/Kolkata')->subMinutes(59)->setTimezone('Asia/Riyadh')->format('Y-m-d H:i:s');
         }
 
         if ($end_time != null) {
-            $end_time = Carbon::now()->format('Y-m-d H:i:s');
+            //$end_time = Carbon::now()->format('Y-m-d H:i:s');
             //$end_time = '2024-07-31 09:59:59';
+            $end_time = Carbon::now('Asia/Kolkata')->setTimezone('Asia/Riyadh')->format('Y-m-d H:i:s');
         }
 
         return array(
