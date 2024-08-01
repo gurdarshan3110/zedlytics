d889722693a21c2f732328ec2ad676b2fd1e4bc0
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-08-01 17:23:22 +0400

    dashboard and rms

--- resources/views/riskmanagement/list.blade.php
+++ resources/views/riskmanagement/list.blade.php
@@ -30,3 +30,53 @@
         </table>
     </div>
 </div>
+@push('jsscript')
+    <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var table = $('#top-table-winners').DataTable({
+            dom: 'Bfrtip',
+            pageLength: 15,
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    title: 'wl-report'
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: 'wl-report'
+                },
+                {
+                    extend: 'csvHtml5',
+                    title: 'wl-report'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: 'wl-report'
+                }
+            ],
+            drawCallback: function () {
+                $('.dataTables_filter input').addClass('form-control form-control-solid w-250px');
+                $('.dt-buttons button').addClass('fs-7 active-menu-item text-light border-0');
+                $('.dt-buttons').addClass('mb-2');
+                $('.dt-button').addClass('btn-primary');
+                $('.paginate_button').addClass('fs-7');
+                $('.paginate_button.current').addClass('fs-7 active-menu-item');
+                $('.paginate_button.active-menu-item').removeClass('current');
+            },
+        });
+
+    });
+
+</script>
+@endpush
