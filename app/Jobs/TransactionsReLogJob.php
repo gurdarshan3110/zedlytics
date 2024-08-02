59d04e0b451337e9caad41c80b56e04e9a7709a3
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-08-02 14:31:47 +0400

    Base Currency API

--- app/Jobs/TransactionsReLogJob.php
+++ app/Jobs/TransactionsReLogJob.php
@@ -93,15 +93,15 @@ class TransactionsReLogJob implements ShouldQueue
         }
 
         if ($start_time != null) {
-            //$start_time = Carbon::parse($end_time)->format('Y-m-d H:i:s');
+            $start_time = Carbon::parse($end_time)->format('Y-m-d H:i:s');
             //$start_time = Carbon::now('Asia/Kolkata')->subHours(2)->setTimezone('Asia/Riyadh')->format('Y-m-d H:i:s');
-            $start_time = '2024-08-02 07:30:00';
+            //$start_time = '2024-08-02 07:30:00';
         }
 
         if ($end_time != null) {
-            //$end_time = Carbon::parse($end_time)->addMinutes(3)->format('Y-m-d H:i:s');
+            $end_time = Carbon::parse($end_time)->addMinutes(3)->format('Y-m-d H:i:s');
             //$end_time = Carbon::now('Asia/Kolkata')->setTimezone('Asia/Riyadh')->format('Y-m-d H:i:s');
-            $end_time = '2024-08-02 08:00:00';
+            //$end_time = '2024-08-02 08:00:00';
         }
 
         return array(
