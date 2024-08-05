90d1fa7cb3acb2b2dcea8fa068146033f6a3b54a
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-08-05 14:13:28 +0400

    Market Changes

--- app/Http/Controllers/RiskManagementController.php
+++ app/Http/Controllers/RiskManagementController.php
@@ -89,7 +89,7 @@ class RiskManagementController extends Controller
         foreach ($parentCurrencies as $parent) {
             
             $childCurrencies = $parent->childCurrencies;
-            $trxLogs = TrxLog::whereIn('currencyId', $childCurrencies->pluck('base_id'))
+            $trxLogs = TrxLog::whereIn('currencyId', $childCurrencies->pluck('base_id'))->where('used',1)
                  ->whereBetween('createdDate', [$startDate, $endDate])->get();
             $totalCloseProfit = $trxLogs->sum('closeProfit');
 
