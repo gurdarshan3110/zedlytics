3a2910d55cf6fe08e4ce95abdf4481c74b251438
Gurdarshan Singh <gurdarshan3110@gmail.com>
2024-08-09 15:37:42 +0400

    trx indexes

---
+++ database/migrations/2024_08_09_113558_add_indexes_to_trx_logs.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trx_logs', function (Blueprint $table) {
+            $table->index('createdDate');
+            $table->index('userId');
+            $table->index('accountId');
+            $table->index('parentId');
+            $table->index('currencyId');
+
+            $table->index('closeProfit');
+            $table->index('openCommission');
+            $table->index('closeCommission');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trx_logs', function (Blueprint $table) {
+            $table->dropIndex(['createdDate']);
+            $table->dropIndex(['userId']);
+            $table->dropIndex(['accountId']);
+            $table->dropIndex(['parentId']);
+            $table->dropIndex(['currencyId']);
+
+            $table->dropIndex(['closeProfit']);
+            $table->dropIndex(['openCommission']);
+            $table->dropIndex(['closeCommission']);
+        });
+    }
+};
